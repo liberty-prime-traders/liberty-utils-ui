6797a3eb70bb0c9dad63fa6abb6ad42c4d815d04
Ezra Orina <Ezra.Orina@nisc.coop>
2025-05-02 01:38:10 -0500

    Got it all done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   //production: false,
-  BASE_URL: 'http://daily-snapshot:8080',
+  BASE_URL: 'https://daily-snapshot:8080',
   // BASE_URL: 'http://localhost:8080',
   OKTA_DOMAIN: 'okta.web-solutions.ezra-home.me/oauth2/default',
   OKTA_CLIENT_ID: '0oaojr54maSYqZXu45d7'
