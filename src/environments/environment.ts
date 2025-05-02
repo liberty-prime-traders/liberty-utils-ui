f7279d88272d52429ea18c6bea0ae56ddb9ab580
Ezra Orina <Ezra.Orina@nisc.coop>
2025-05-02 11:05:01 -0500

    change base url to prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   //production: false,
-  BASE_URL: 'https://daily-snapshot:8080',
+  BASE_URL: 'https://daily-snapshot-server.ezra-home.me',
   // BASE_URL: 'http://localhost:8080',
   OKTA_DOMAIN: 'okta.web-solutions.ezra-home.me/oauth2/default',
   OKTA_CLIENT_ID: '0oaojr54maSYqZXu45d7'
