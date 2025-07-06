84019c59e9257b98b4306e185aed88c346bfec9a
kenneth <kennethbuchunju@gmail.com>
2025-07-06 03:41:14 +0300

    added transaction model service and store

---
+++ src/api/transactions/transaction.model.ts
@@ -0,0 +1,11 @@
+import {BaseModel} from '../base-api/base.model'
+import {TransactionType} from './transaction-type.enum'
+
+export interface Transaction extends BaseModel{
+  userID?: string;
+  contactName?: string,
+  transactionType?: TransactionType,
+  transactionDate?: number,
+  amount?: number,
+  description?: string
+}
