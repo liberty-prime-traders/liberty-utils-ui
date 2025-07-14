4b76eafa774ef2af71b1caa57c95315a813ead25
Ezra Orina <Ezra.Orina@nisc.coop>
2025-07-14 00:56:20 -0500

    change backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   // production: false,
-  BASE_URL: 'https://liberty-utils.ezra-home.me',
+  BASE_URL: 'https://liberty-utils-server.ezra-home.me',
   // BASE_URL: 'http://localhost:8080',
   OKTA_DOMAIN: 'okta.liberty-solutions.ezra-home.me/oauth2/default',
   OKTA_CLIENT_ID: '0oat6t2t3vXAZfI4f697'
