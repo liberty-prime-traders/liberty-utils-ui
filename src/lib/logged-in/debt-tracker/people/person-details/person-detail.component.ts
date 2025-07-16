5a4c83c41c0358a07c892676ed42ff6ebe6b4834
kenneth <kennethbuchunju@gmail.com>
2025-07-16 20:57:00 +0300

    Updating users

--- src/lib/logged-in/debt-tracker/people/person-details/person-detail.component.ts
+++ src/lib/logged-in/debt-tracker/people/person-details/person-detail.component.ts
@@ -1,16 +1,23 @@
-import {Component, computed, inject, OnInit, Signal} from '@angular/core';
+import {Component, computed, inject, input, OnInit, signal, Signal, WritableSignal} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ContactService} from '../../../../../api/contacts/contact.service';
 import {TransactionService} from '../../../../../api/transactions/transaction.service';
 import {Contact} from '../../../../../api/contacts/contact.model';
-import {CurrencyPipe, DatePipe, formatCurrency} from '@angular/common';
+import {AsyncPipe, CurrencyPipe, DatePipe, formatCurrency, NgIf} from '@angular/common';
 import {Card} from 'primeng/card';
 import {PrimeTemplate} from 'primeng/api';
 import {DropdownModule} from 'primeng/dropdown';
 import {TableModule} from 'primeng/table';
 import {TransactionType} from '../../../../../api/transactions/transaction-type.enum';
 import {Transaction} from '../../../../../api/transactions/transaction.model';
-import {FormsModule} from '@angular/forms';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {LbuOktaService} from '../../../../../config/lbu-okta.service';
+import {Button, ButtonDirective} from 'primeng/button';
+import {EnumToDropdownPipe} from '../../../../pipes/enum-to-dropdown.pipe';
+import {InputText} from 'primeng/inputtext';
+import {Select} from 'primeng/select';
+import {ContactType} from '../../../../../api/contacts/contact-type.enum';
+import {Dialog} from 'primeng/dialog';
 
 @Component({
   selector: 'dbt-person-detail',
@@ -23,7 +30,16 @@ import {FormsModule} from '@angular/forms';
     TableModule,
     DatePipe,
     CurrencyPipe,
-    FormsModule
+    FormsModule,
+    NgIf,
+    AsyncPipe,
+    Button,
+    ButtonDirective,
+    EnumToDropdownPipe,
+    InputText,
+    ReactiveFormsModule,
+    Select,
+    Dialog
   ]
 })
 export class PersonDetailComponent implements OnInit {
@@ -32,9 +48,14 @@ export class PersonDetailComponent implements OnInit {
   private transactionService = inject(TransactionService);
   protected readonly TransactionType = TransactionType;
   protected readonly formatCurrency = formatCurrency;
+  readonly dspOktaService = inject(LbuOktaService);
+  protected readonly ContactType = ContactType;
+  private readonly formBuilder = inject(FormBuilder);
+  readonly contact = input<Contact>();
+  visible: boolean | WritableSignal<boolean>  = false;
 
-  personId!: string;
-  person?: Contact;
+  personId = signal<string>('');
+  person!: Signal<Contact | undefined>;
   transactions!: Signal<Transaction[]>;
 
   sortOptions = [
@@ -46,17 +67,30 @@ export class PersonDetailComponent implements OnInit {
 
   selectedSortOrder = 'date_desc';
 
+  readonly contactForm = computed(() => this.formBuilder.nonNullable.group({
+    id: this.person()?.id,
+    fullName: [this.person()?.fullName, Validators.required],
+    email: [this.person()?.email],
+    phoneNumber: [this.person()?.phoneNumber],
+    contactType: [this.person()?.contactType, Validators.required]
+  }));
 
   ngOnInit(): void {
-    this.personId = this.route.snapshot.paramMap.get('id') ?? '';
+    this.route.paramMap.subscribe(params => {
+      this.personId.set(params.get('id') ?? '');
+    });
+
     if (!this.personId) return;
 
-    this.person = this.contactService.selectAll().find(p => p.id === this.personId);
+    this.person = computed(() =>
+      this.contactService.selectAll().find(p => p.id === this.personId())
+    );
 
     this.transactions = computed(() =>
-      this.transactionService.selectAll().filter(t => t.userID === this.personId)
+      this.transactionService.selectAll().filter(t => t.userID === this.personId())
     );
   }
+
 
   getInitials(fullName: string): string {
     if (!fullName) return '';
@@ -103,4 +137,19 @@ export class PersonDetailComponent implements OnInit {
       }
     });
   });
+
+  onEdit() {
+    this.visible = true;
+  }
+
+  saveContact() {
+    const updatedContact: Partial<Contact> = {...this.contactForm().getRawValue(), id: this.personId()};
+    this.contactService.put(updatedContact)
+    this.onCancel()
+  }
+
+  onCancel() {
+    this.contactForm().reset(this.person())
+    this.visible = false
+  }
 }
