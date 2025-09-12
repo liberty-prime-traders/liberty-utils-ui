12d30740c463e6fbadf98d992d88f90806938a63
kenneth <kennethbuchunju@gmail.com>
2025-09-12 22:50:37 +0300

    caching the GET response

--- src/api/contact-transactions/contact-transaction.service.ts
+++ src/api/contact-transactions/contact-transaction.service.ts
@@ -1,4 +1,4 @@
-import {inject, Injectable} from '@angular/core'
+import {inject, Injectable, signal} from '@angular/core'
 import {BaseService} from '../base-api/base.service'
 import {Transaction} from '../transactions/transaction.model'
 import {ContactTransactionStore} from './contact-transaction.store'
@@ -8,6 +8,7 @@ import {HttpParams} from '@angular/common/http'
 @Injectable({providedIn: 'root'})
 export class ContactTransactionService extends BaseService<Transaction> {
   private readonly contactTransactionsCache = new Map<string, Transaction[]>()
+  private readonly latestQuery = signal('')
 
   constructor() {
     super(inject(ContactTransactionStore))
@@ -19,12 +20,33 @@ export class ContactTransactionService extends BaseService<Transaction> {
       this.store.setAll(this.contactTransactionsCache.get(key)!)
       return undefined
     }
+    this.latestQuery.set(key)
     return super.refetch(params)
   }
 
   override getHttpParams(params: {userId: string}): HttpParams {
     return new HttpParams()
       .setNonNull('userId', params.userId)
+  }
+
+  override finishSavingWithSuccess(response: Transaction | Transaction[]): void {
+    if (Array.isArray(response)) {
+      this.contactTransactionsCache.set(this.latestQuery(), response)
+    } else {
+      this.upsertTransactionInCache(response)
+    }
+    super.finishSavingWithSuccess(response)
+  }
+
+  private upsertTransactionInCache(transaction: Transaction): void {
+    this.contactTransactionsCache.forEach((cached, _) => {
+      const index = cached.findIndex(t => t.id === transaction.id)
+      if (index !== -1) {
+        cached[index] = transaction
+      } else {
+        cached.unshift(transaction)
+      }
+    })
   }
 
 }
