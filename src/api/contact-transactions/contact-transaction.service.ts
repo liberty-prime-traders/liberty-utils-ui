b0993a0e3dca07861daa206f122fa9c3bd418e6d
Ezra Orina <Ezra.Orina@nisc.coop>
2025-09-24 01:36:52 -0500

    refactor

--- src/api/contact-transactions/contact-transaction.service.ts
+++ src/api/contact-transactions/contact-transaction.service.ts
@@ -1,4 +1,4 @@
-import {inject, Injectable, signal} from '@angular/core'
+import {inject, Injectable} from '@angular/core'
 import {FetchService} from '../base-api/fetch-service'
 import {Transaction} from '../transactions/transaction.model'
 import {ContactTransactionStore} from './contact-transaction.store'
@@ -9,7 +9,6 @@ import {ProcessingStatus} from '../processing-status.enum'
 @Injectable({ providedIn: 'root'})
 export class ContactTransactionService extends FetchService<Transaction> {
   private readonly contactTransactionsCache = new Map<string, Transaction[]>()
-  private readonly $latestUserIdQueried = signal('')
 
   constructor() {
     super(inject(ContactTransactionStore), inject(HttpClient))
@@ -24,7 +23,6 @@ export class ContactTransactionService extends FetchService<Transaction> {
       return undefined
     }
 
-    this.$latestUserIdQueried.set(key)
     return super.refetch(params)
   }
 
@@ -33,16 +31,22 @@ export class ContactTransactionService extends FetchService<Transaction> {
   }
 
   override finishSavingWithSuccess(response: Transaction[]): void {
-    this.contactTransactionsCache.set(this.$latestUserIdQueried(), response)
+    const userId = response[0].userId!
+    this.contactTransactionsCache.set(userId, response)
     super.finishSavingWithSuccess(response)
   }
 
   public upsertTransactionInCache(transaction: Transaction): void {
-    this.contactTransactionsCache.forEach((cached) => {
-      const index = cached.findIndex(t => t.id === transaction.id)
+    const userId = transaction.userId!
+    if (this.contactTransactionsCache.has(userId)) {
+      const cachedTransactions = this.contactTransactionsCache.get(userId)!
+      const index = cachedTransactions.findIndex(t => t.id === transaction.id)
       if (index !== -1) {
-        cached[index] = transaction
+        cachedTransactions[index] = transaction
+      } else {
+        cachedTransactions.unshift(transaction)
       }
-    })
+      this.contactTransactionsCache.set(userId, cachedTransactions)
+    }
   }
 }
