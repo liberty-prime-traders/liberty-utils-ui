e9071b2dbd1bdb048188c9b3848be8effe4d69ad
Ezra Orina <Ezra.Orina@nisc.coop>
2026-02-06 12:00:01 -0600

    remove dev deployment

--- deploy-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ng build --configuration development
-docker build --platform linux/arm64 -t liberty-utils-ui .
-docker tag liberty-utils-ui ezraorina834/liberty-utils-ui:dev
-docker push ezraorina834/liberty-utils-ui:dev
--- docker/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ACTION="${1:-up}"  # default to up
-
-if [[ "$ACTION" == "down" ]]; then
-  docker compose --env-file dev.env down
-else
-  docker compose --env-file dev.env up -d
-fi
