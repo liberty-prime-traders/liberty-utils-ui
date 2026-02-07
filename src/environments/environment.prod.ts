a5b9402b012581c772fb9ea196e59933128309dc
Ezra Orina <Ezra.Orina@nisc.coop>
2026-02-06 18:50:31 -0600

    put server back in prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { COMMON_ENV } from './environment.common'
 
 export const environment = {
-  BASE_URL: 'https://liberty-utils-prod.ezra-home.me',
+  BASE_URL: 'https://liberty-utils-server-prod.ezra-home.me',
   ...COMMON_ENV,
 }
